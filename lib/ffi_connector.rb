2451e373d7ad4843349f504aaed2a445e60106eb
Ian Smith <ianhenrysmith@gmail.com>
2016-02-25 07:06:39 -0700

    use unsigned long long for bigger numbers

--- lib/ffi_connector.rb
+++ lib/ffi_connector.rb
@@ -4,9 +4,9 @@ module FfiConnector
   extend FFI::Library
   ffi_lib "c"
   ffi_lib "#{Rails.root}/lib/ffi_test.so"
-  attach_function :ffi_pow, [ :int, :int ], :int
-  attach_function :ffi_factorial, [ :int ], :int
-  attach_function :ffi_fibonacci, [ :int ], :int
+  attach_function :ffi_pow, [ :ulong_long, :ulong_long ], :ulong_long
+  attach_function :ffi_factorial, [ :ulong_long ], :ulong_long
+  attach_function :ffi_fibonacci, [ :ulong_long ], :ulong_long
 
   # int => ulong_long
   # http://www.rubydoc.info/github/ffi/ffi/FFI/TypesGenerator
